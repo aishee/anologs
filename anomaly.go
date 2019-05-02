5cac100f7fd73295535326597951053b4014640f
Aishee Nguyen <aishee@aishee.net>
2019-05-02 14:00:53 +0700

    Update

--- anomaly.go
+++ anomaly.go
@@ -1,5 +1,14 @@
 package anologs
 
+import (
+	"bufio"
+  "log"
+  "fmt"
+  "github.com/jessevdk/go-flags"
+	anologs "github.com/aishee/anologs/utils"
+	"os"
+)
+
 type options struct {
 	InputModel  string `short:"i" long:"input"`
 	OutputModel string `short:"o" long:"output"`
@@ -13,11 +22,34 @@ func readFile(fpath string, engine *anologs.Engine) error {
 	}
 	defer fp.Close()
 	s := bufio.NewScanner(fp)
-  for s.Scan() {
-    text := s.Text()
-    if len(text) >0 {
-      engine.Read(text)
-    }
-  }
+	for s.Scan() {
+		text := s.Text()
+		if len(text) > 0 {
+			engine.Read(text)
+		}
+	}
+	return nil
+}
 
+func main() {
+	var opts options
+	args, ParseErr := flags.ParseArgs(&opts, os.Args)
+	if ParseErr != nil {
+		os.Exit(1)
+	}
+	engine := anologs.NEngine()
+	if opts.InputModel != "" {
+		engine.Load(opts.InputModel)
+	}
+	for _, fpath := range args[1:] {
+		log.Println("Reading file...", fpath)
+		readFile(fpath, &engine)
+	}
+  log.Println("Done")
+  for idx, format := range engine.Formats() {
+    fmt.Printf("[%2d] %s\n", idx, format.String())
+  }
+  if opts.OutputModel != "" {
+    engine.Save(opts.OutputModel)
+  }
 }
